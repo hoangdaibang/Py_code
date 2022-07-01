aed419dfae641de1a2fee7cf43e1862221661e1e
Hoàng Đại Bằng <57615300+hoangdaibang@users.noreply.github.com>
2022-07-01 16:30:24 +0700

    Update lambda.py

--- Tutorial/Python_tutorial_#12/lambda.py
+++ Tutorial/Python_tutorial_#12/lambda.py
@@ -33,17 +33,17 @@ custom sorrting using a lamda funtion as key parameter
 
 '''
 #ví dụ 
-from ast import Lambda
-from distutils.ccompiler import new_compiler
-from functools import reduce
+# from ast import Lambda
+# from distutils.ccompiler import new_compiler
+# from functools import reduce
 
 
-sample_dict = {(1, 12, 8),(3, 9, 7),(8, 8, 11)}
-print(sorted(sample_dict,key= lambda el : el[0]))
-#hoặc là sắp xếp theo giá trị tuyệt đối của tùng phần tử trong list
-sample_list = [6, -5, 8, 6, -4, -7, 1, -6]
-print(sorted(sample_list,key= lambda x : abs(x)))
-'''
+# sample_dict = {(1, 12, 8),(3, 9, 7),(8, 8, 11)}
+# print(sorted(sample_dict,key= lambda el : el[0]))
+# #hoặc là sắp xếp theo giá trị tuyệt đối của tùng phần tử trong list
+# sample_list = [6, -5, 8, 6, -4, -7, 1, -6]
+# print(sorted(sample_list,key= lambda x : abs(x)))
+# '''
 
 sử dụng lamda trong hàm map
 hàm map giúp chuyển đổi từng phần tử của một List bằng một hàm nào đó
@@ -70,13 +70,13 @@ filter funtion return all elements for which funtion return True
 
 '''
 #ví dụ
-list_number = [1, 3 , 7 , 9,  11, 12, 4 , 7, 8, 2]
-print(list(filter(lambda x : x % 2 == 0, list_number)))
+#list_number = [1, 3 , 7 , 9,  11, 12, 4 , 7, 8, 2]
+#print(list(filter(lambda x : x % 2 == 0, list_number)))
 
 #và trong trường hợp này hàm filter cũng được có tác dung như một list comprehension khác 
 
-new_list_number = [x for x in list_number if x % 2 == 0]
-print(new_list_number)
+# new_list_number = [x for x in list_number if x % 2 == 0]
+# print(new_list_number)
 #và tức nhiên kết quả trả về cung chả khác gì nhau cả 
 '''
 dùng lambda trong hàm reduce
@@ -84,9 +84,9 @@ Hàm reduce áp dụng hàm cho từng phần tử trong chuỗi cho đến hế
 
 '''
 #ví dụ 
-sequence = [1, 4, 7 , 3, 5, 13]
-#tính tổng các phần tử trong chuỗi 
-print(reduce(lambda a , b : a + b, sequence))
-#tìm giá trị lớn nhất 
-print(reduce(lambda a, b : a if a > b else b, sequence))
+# sequence = [1, 4, 7 , 3, 5, 13]
+# #tính tổng các phần tử trong chuỗi 
+# print(reduce(lambda a , b : a + b, sequence))
+# #tìm giá trị lớn nhất 
+# print(reduce(lambda a, b : a if a > b else b, sequence))
 
